c1f171d7c74891e308d00cd6e1e7c7463485f228
IRUTABYOSE Yoramu <iyoramu@gmail.com>
2025-06-04 12:32:25 +0200

    build(android): restructure build configuration and add project dependency
    - Remove redundant buildscript block as it's now defined in root project
    - Add evaluation dependency for subprojects on app module to ensure proper build order

--- mobile-app/android/build.gradle.kts
+++ mobile-app/android/build.gradle.kts
@@ -1,14 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -22,6 +11,9 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {

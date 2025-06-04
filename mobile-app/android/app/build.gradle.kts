58009a969cfebf388a7f74d9255c8aec4331270f
IRUTABYOSE Yoramu <iyoramu@gmail.com>
2025-06-04 18:24:22 +0200

    build(android): downgrade NDK version to 25.1.8937393
    The NDK version was downgraded to maintain compatibility with other dependencies in the project that require this specific version. This change ensures consistent build behavior across all development environments.

--- mobile-app/android/app/build.gradle.kts
+++ mobile-app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.curry_puff_master"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.2.9519653"
+    ndkVersion = "25.1.8937393"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
